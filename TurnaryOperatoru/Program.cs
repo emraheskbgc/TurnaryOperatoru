a146b09d4d400f11a4ce19cc2b97f3a12017947c
emrah eskibağcı <emraheskibagci595@gmail.com>
2021-12-10 22:22:55 +0300

    Add project files.

---
+++ TurnaryOperatoru/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnaryOperatoru
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int sonuc = sayi < 3 ? sayi * 5 : sayi > 3 && sayi < 9 ? sayi * 3 :
+                sayi >= 9 && sayi % 2 == 0 ? sayi * 10 : sayi % 2 == 1 ? sayi : -1;
+            Console.WriteLine(sonuc);
+            
+
+        }
+    }
+}
